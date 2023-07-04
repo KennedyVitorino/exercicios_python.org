0c6de14f050507cf23b929301869fab8b0253928
Kennedy <dkennyvs@gmail.com>
2023-07-04 20:48:25 -0300

    Conversor de metros para centímetros.

---
+++ lista_de_exercicios/estruturas_sequenciais/05_conversor_metros_centimetros.py
@@ -0,0 +1,12 @@
+# 5. Faça um Programa que converta metros para centímetros
+
+
+def converter_metros_para_centimetros(m):
+    c = m * 100
+    return c
+
+
+metros = float(input('Metros à converter: '))
+centimetros = converter_metros_para_centimetros(metros)
+
+print(f'{metros:.1f} metros convertidos para centímetros é -> {centimetros:.1f}cm')
