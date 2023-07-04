a3a8bc6f91617196ec8f900fcadde2adbf2d0eb5
Kennedy <dkennyvs@gmail.com>
2023-07-04 20:48:36 -0300

    Calculo da área de um cículo

---
+++ lista_de_exercicios/estruturas_sequenciais/06_area_do_circulo.py
@@ -0,0 +1,12 @@
+# 6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+
+def calcular_area_circulo(r):
+    a = 3.14159 * (r ** 2)
+    return a
+    
+
+raio = float(input('Raio: '))
+area = calcular_area_circulo(raio)
+
+print(f'A área do círculo é: {area:.1f}')
