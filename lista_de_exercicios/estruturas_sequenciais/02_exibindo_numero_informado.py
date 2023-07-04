22183999afdb0669455476fd11a04fc3d0b88614
Kennedy <dkennyvs@gmail.com>
2023-07-04 20:47:12 -0300

    Exibindo o número informado

---
+++ lista_de_exercicios/estruturas_sequenciais/02_exibindo_numero_informado.py
@@ -0,0 +1,9 @@
+# 2. Faça um Programa que peça um número e então mostre a mensagem O
+# número informado foi [número].
+
+def exibir_numero(n):
+    print(f'O número informado foi -> [{n}]')
+
+
+numero = int(input('Número: '))
+exibir_numero(numero)
