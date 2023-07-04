33079202d4342d9e78e463e2bce5228442261837
Kennedy <dkennyvs@gmail.com>
2023-07-04 20:49:09 -0300

    Calculadora de salário, simples.

---
+++ lista_de_exercicios/estruturas_sequenciais/08_calculadora_salario.py
@@ -0,0 +1,15 @@
+# Faça um Programa que pergunte quanto você ganha por hora
+# e o número de horas trabalhadas no mês. Calcule e mostre
+# o total do seu salário no referido mês.
+
+
+def calculo_salario(valor_hora, horas_trabalho):
+    salario_total = valor_hora * horas_trabalho
+    return salario_total
+
+
+salario_hora = float(input('Valor recebido por hora trabalhada: R$ '))
+horas_trabalhadas = int(input('Horas trabalhadas esse mês: '))
+salario_final = calculo_salario(salario_hora, horas_trabalhadas)
+
+print(f'Salário final: {salario_final:.2f}')
