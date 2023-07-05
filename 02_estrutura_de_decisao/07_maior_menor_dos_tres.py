15bae0a94cf360342cd047b4b46a8c8a8f264344
Kennedy <dkennyvs@gmail.com>
2023-07-05 01:07:39 -0300

    Maior e menor entre três números

--- 02_estrutura_de_decisao/07_maior_menor_dos_tres.py
+++ 02_estrutura_de_decisao/07_maior_menor_dos_tres.py
@@ -1,1 +1,15 @@
 # 7. Faça um Programa que leia três números e mostre o maior e o menor deles.
+
+def encontrar_maior(a, b, c):
+    if a > b and a > c:
+        maior = a
+    elif b > a and b > c:
+        maior = b
+        
+n1 = int(input('n :'))
+n2 = int(input('n :'))
+n3 = int(input('n :'))
+
+ma, me = encontrar_maior_menor(n1, n2, n3)
+
+print(ma, me)
