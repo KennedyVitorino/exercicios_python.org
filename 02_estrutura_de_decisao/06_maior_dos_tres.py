0ae7c87b2bb5ed1e7ac70377fda2bf9b60d3b30b
Kennedy <dkennyvs@gmail.com>
2023-07-05 00:51:53 -0300

    Maior entre três números

--- 02_estrutura_de_decisao/06_maior_dos_tres.py
+++ 02_estrutura_de_decisao/06_maior_dos_tres.py
@@ -3,4 +3,19 @@
 
 def maior_deles(a, b, c):
     m = a
+    if a > b and a > c:
+        m = a
+    elif b > a and b > c:
+        m = b
+    elif c > a and c > b:
+        m = c
+    return m
     
+    
+n1 = int(input('Número 1: '))
+n2 = int(input('Número 2: '))
+n3 = int(input('Número 3: '))
+
+maior = maior_deles(n1, n2, n3)
+
+print(maior)
