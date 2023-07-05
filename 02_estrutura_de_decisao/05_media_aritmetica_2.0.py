1668171c6f7dab35e828a564235aa737a2e0413c
Kennedy <dkennyvs@gmail.com>
2023-07-05 00:43:49 -0300

    Média aritmética 2.0

---
+++ 02_estrutura_de_decisao/05_media_aritmetica_2.0.py
@@ -0,0 +1,25 @@
+# Faça um programa para a leitura de duas notas parciais de um aluno.
+# O programa deve calcular a média alcançada por aluno e apresentar:
+# A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+# A mensagem "Reprovado", se a média for menor do que sete;
+# A mensagem "Aprovado com Distinção", se a média for igual a dez.
+
+
+def media_aritmetica(n1, n2):
+    m = float(n1 + n2) / 2
+    if m < 0:
+        return '⚠️ Digite um valor válido ⚠️'
+    elif m > 10:
+        return '⚠️ Digite um valor válido ⚠️'
+    elif not m != 10:
+        return '🥳👏🏽 APROVADO COM HONRAS 👏🏽🥳'
+    elif m >= 7:
+        return '🥳👏🏽 APROVADO 🥳👏🏽'
+    return '⚠️☠️ REPROVADO ☠️⚠️'
+    
+    
+nota_1 = float(input('Digite a primeira nota: '))
+nota_2 = float(input('Digite a segunda nota: '))
+media = media_aritmetica(nota_1, nota_2)
+
+print(f'\n{media}')
