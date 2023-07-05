16bed5fc4ca91709667723a7612169e8b9fea218
Kennedy <dkennyvs@gmail.com>
2023-07-05 00:23:55 -0300

    verifica gênero (M/F)

--- 02_estrutura_de_decisao/03_verifica_genero_digitado.py
+++ 02_estrutura_de_decisao/03_verifica_genero_digitado.py
@@ -5,14 +5,15 @@
 
 def verifica_genero(g):
     g = g.lower()
-    if g == 'm':
-        return 'Gênero [M]asculino'
-    elif g == 'f':
-        return 'Gênero [F]eminino'
+    if len(g) == 1 and g.isalpha():
+        if g == 'm':
+            return 'Gênero [M]asculino'
+        elif g == 'f':
+            return 'Gênero [F]eminino'
     return 'Gênero Inválido.'
 
 
 genero = input('digite o gênero: ')
 verifica_genero = verifica_genero(genero)
 
-print(verifica_genero)
+print(f'{verifica_genero}')
