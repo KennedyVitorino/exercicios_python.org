43b8ee0762d7526d6f58e43643f1726aa30a3e61
Kennedy <dkennyvs@gmail.com>
2023-07-04 23:40:48 -0300

    maior entre dois números

--- 02_estrutura_de_decisao/01_maior_entre_dois_numeros.py
+++ 02_estrutura_de_decisao/01_maior_entre_dois_numeros.py
@@ -1,1 +1,15 @@
 # 1. Faça um Programa que peça dois números e imprima o maior deles.
+
+def numero_maior(num1, num2):
+    if num1 > num2:
+        maior = num1
+    else:
+        maior = num2
+    return maior
+    
+
+n1 = int(input('Número 1: '))
+n2 = int(input('Número 2: '))
+
+maior_numero = numero_maior(n1, n2)
+print(maior_numero)
