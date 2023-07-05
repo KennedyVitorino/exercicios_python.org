a7f895afb7e9941b274e01b3c2804b7cf7f96ea6
Kennedy <dkennyvs@gmail.com>
2023-07-05 15:19:02 -0300

    Números em ordem decrescente

--- 02_estrutura_de_decisao/09_numeros_em_ordem_crescente.py
+++ 02_estrutura_de_decisao/09_numeros_em_ordem_crescente.py
@@ -2,39 +2,39 @@
 
 
 def ordenar_numeros(num1, num2, num3):
-    if num1 <= num2 and num1 <= num3:
-        menor = num1
-        if num2 <= num3:
-            segundo_menor = num2
-            maior = num3
+    if num1 >= num2 and num1 >= num3:
+        maior = num1
+        if num2 >= num3:
+            segundo_maior = num2
+            menor = num3
         else:
-            segundo_menor = num3
-            maior = num2
-    elif num2 <= num1 and num2 <= num3:
-        menor = num2
-        if num1 <= num3:
-            segundo_menor = num1
-            maior = num3
+            segundo_maior = num3
+            menor = num2
+    elif num2 >= num1 and num2 >= num3:
+        maior = num2
+        if num1 >= num3:
+            segundo_maior = num1
+            menor = num3
         else:
-            segundo_menor = num3
-            maior = num1
+            segundo_maior = num3
+            menor = num1
     else:
-        menor = num3
-        if num1 <= num2:
-            segundo_menor = num1
-            maior = num2
+        maior = num3
+        if num1 >= num2:
+            segundo_maior = num1
+            menor = num2
         else:
-            segundo_menor = num2
-            maior = num1
+            segundo_maior = num2
+            menor = num1
     
-    numeros_ordem_crescente = [menor, segundo_menor, maior]
-    return numeros_ordem_crescente
+    numeros_ordem_decrescente = [maior, segundo_maior, menor]
+    return numeros_ordem_decrescente
 
 
 n1 = float(input('Digite o primeiro número: '))
 n2 = float(input('Digite o segundo número: '))
 n3 = float(input('Digite o terceiro número: '))
 numeros_ordenados = ordenar_numeros(n1, n2, n3)
-ordem_crescente = f'Em ordem crescente: {", ".join(map(str, numeros_ordenados))}'
+ordem_decrescente = f'Em ordem decrescente: {", ".join(map(str, numeros_ordenados))}'
 
-print(ordem_crescente)
+print(ordem_decrescente)
