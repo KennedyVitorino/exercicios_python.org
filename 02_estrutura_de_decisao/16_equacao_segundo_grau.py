f4130fdc97257d186dd2b88e8a8b92c5786ec1e4
Kennedy <dkennyvs@gmail.com>
2023-07-07 16:59:01 -0300

    Raízes de uma equação de segundo grau

--- 02_estrutura_de_decisao/16_equacao_segundo_grau.py
+++ 02_estrutura_de_decisao/16_equacao_segundo_grau.py
@@ -8,3 +8,32 @@
 # Se o delta calculado for igual a zero a equação possui apenas uma raiz real;
 # informe-a ao usuário;
 # Se o delta for positivo, a equação possui duas raiz reais; informe-as ao usuário;
+from math import sqrt
+
+
+def calculo_raizes_equacao_segundo_grau(a, b, c):
+    if a == 0:
+        return 'Não é equação do segundo grau.'
+    
+    delta = b ** 2 - 4 * a * c
+    if delta < 0:
+        'A equação não possui raízes reais.'
+    elif delta == 0:
+        raiz = -b / (2 * a)
+        return f'A equação possui uma rais real: {raiz}'
+    else:
+        raiz1 = (-b + sqrt(delta)) / (2 * a)
+        raiz2 = (-b + sqrt(delta)) / (2 * a)
+        return f'A equação possui duas raízes reais: {raiz1} - {raiz2}'
+
+     
+n1 = float(input('Digite o valor de a: '))
+if n1 != 0:
+    n2 = float(input('Digite o valor de b: '))
+    n3 = float(input('Digite o valor de c: '))
+    
+    resultado = calculo_raizes_equacao_segundo_grau(n1, n2, n3)
+    print(resultado)
+else:
+    print('A equação não é o segundo grau')
+    
