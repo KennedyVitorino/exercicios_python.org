7725f75b2b023c46aea3b2a2cb7f2260c81c381f
Kennedy <dkennyvs@gmail.com>
2023-07-08 00:18:13 -0300

    Par ou ímpar

--- 02_estrutura_de_decisao/22_verificar_par_impar.py
+++ 02_estrutura_de_decisao/22_verificar_par_impar.py
@@ -1,4 +1,4 @@
-# Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica:
+# 22. Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica:
 # utilize o operador módulo (resto da divisão).
 
 
