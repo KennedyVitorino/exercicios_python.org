d1de34d36393c815f4cb5eea1878783b63124ec3
Kennedy <dkennyvs@gmail.com>
2023-07-07 21:29:55 -0300

    Validador "manual" de datas

--- 02_estrutura_de_decisao/18_validador_de_data.py
+++ 02_estrutura_de_decisao/18_validador_de_data.py
@@ -1,2 +1,64 @@
 # Faça um Programa que peça uma data no formato dd/mm/aaaa e determine
 # se a mesma é uma data válida
+from datetime import datetime
+
+
+def validar_data_datetime(data_1):
+    try:
+        data_objeto = datetime.strptime(data_1, '%d/%m/%Y')
+        if data_objeto.year >= 1:
+            if data_objeto <= datetime.now():
+                return True
+    except ValueError:
+        pass
+    
+    return False
+
+
+def data_valida():
+    data = input('Digite uma data no formato dd/mm/aaaa: ')
+    dia, mes, ano = map(int, data.split('/'))
+    
+    # Verificações iniciais.
+    if ano < 1:
+        return False
+    if not 1 <= mes <= 12:
+        return False
+    
+    # Verificações específicar para cada mês.
+    if mes in [1, 3, 5, 7, 8, 10, 12]:
+        if not 1 <= dia <= 31:
+            return False
+    elif mes in [4, 6, 9, 11]:
+        return False
+    elif mes == 2:
+        # Verificano se o ano é BISSEXTO
+        if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+            if not 1 <= dia <= 29:
+                return False
+        else:
+            if not 1 <= dia < 28:
+                return False
+    else:
+        return False
+    
+    return True
+
+
+def main():
+    data_verificar = input('Digite a data no formato dd/mm/aaaa: ')
+    validar_data = validar_data_datetime(data_verificar)
+    
+    if validar_data:
+        print('A data é válida.')
+    else:
+        print('A data é inválida.')
+    
+    if data_valida():
+        print('A data é válida!')
+    else:
+        print('A data é inválida')
+
+
+if __name__ == '__main__':
+    main()
