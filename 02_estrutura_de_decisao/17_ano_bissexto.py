ea3790f88502908e251162cfc55e3cf845d494ca
Kennedy <dkennyvs@gmail.com>
2023-07-07 22:00:47 -0300

    Verificar se o ano bissexto

--- 02_estrutura_de_decisao/17_ano_bissexto.py
+++ 02_estrutura_de_decisao/17_ano_bissexto.py
@@ -1,4 +1,4 @@
-# Faça um Programa que peça um número correspondente a um determinado ano e em seguida
+# 17. aça um Programa que peça um número correspondente a um determinado ano e em seguida
 # informe se este ano é ou não bissexto.
 
 
