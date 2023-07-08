05e0403511fc90949db1a26fb05e2eab6be4e407
Kennedy <dkennyvs@gmail.com>
2023-07-07 22:00:54 -0300

    Validador "manual" de datas

--- 02_estrutura_de_decisao/18_validador_de_data.py
+++ 02_estrutura_de_decisao/18_validador_de_data.py
@@ -1,4 +1,4 @@
-# Faça um Programa que peça uma data no formato dd/mm/aaaa e determine
+# 18. Faça um Programa que peça uma data no formato dd/mm/aaaa e determine
 # se a mesma é uma data válida
 from datetime import datetime
 
