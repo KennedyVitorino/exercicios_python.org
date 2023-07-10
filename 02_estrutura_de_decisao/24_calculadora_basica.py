cb3dc4664518b26f743d52b3491a73210bb086e6
Kennedy <dkennyvs@gmail.com>
2023-07-10 09:05:27 -0300

    Calculadora básica

---
+++ 02_estrutura_de_decisao/24_calculadora_basica.py
@@ -0,0 +1,61 @@
+# 24. Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação
+# ele deseja realizar. O resultado da operação deve ser acompanhado de uma frase que diga
+# se o número é:
+# par ou ímpar;
+# positivo ou negativo;
+# inteiro ou decimal.
+
+
+def par_impar(n):
+    if n % 2 == 0:
+        return 'par'
+    else:
+        return 'ímpar'
+
+
+def positivo_negativo(n):
+    if n > 0:
+        return 'positivo 👍'
+    else:
+        return 'negativo 👎'
+
+
+def inteiro_decimal(n):
+    if n % 1 == 0:
+        return 'inteiro'
+    else:
+        return 'decimal'
+
+
+def calculadora_basica(n1, n2, o):
+    if o == '+':
+        return n1 + n2
+    elif o == '-':
+        return n1 - n2
+    elif o == '*':
+        return n1 * n2
+    elif o == '/':
+        return n1 / n2
+    elif o == '//':
+        return n1 // n2
+    elif o == '**':
+        return n1 ** n2
+    else:
+        raise ValueError('Operação inválida.')
+
+
+def main():
+    numero1 = float(input('Digite o primeiro número: '))
+    numero2 = float(input('Digite o segundo número: '))
+    operacao = input('Escolha uma operação (+, -, *, /, //, **): ')
+    
+    resultado = calculadora_basica(numero1, numero2, operacao)
+    
+    print(f'Resultado: {resultado:.1f}'
+          f'\nParidade: {par_impar(resultado)}'
+          f'\nPositividade: {positivo_negativo(resultado)}'
+          f'\nTipo: {inteiro_decimal(resultado)}')
+
+
+if __name__ == '__main__':
+    main()
