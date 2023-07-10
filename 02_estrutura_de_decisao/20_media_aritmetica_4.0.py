8a0ba4686dbbeb0877aa16aae3a05d2a84f813ce
Kennedy <dkennyvs@gmail.com>
2023-07-10 11:48:38 -0300

    Média aritmética 4.0

--- 02_estrutura_de_decisao/20_media_aritmetica_4.0.py
+++ 02_estrutura_de_decisao/20_media_aritmetica_4.0.py
@@ -23,6 +23,6 @@ nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 nota3 = float(input('Digite a terceira nota: '))
 
-media_geral = media_aluno(nota1, nota2, nota3)
+media_final = media_aluno(nota1, nota2, nota3)
 
-print(media_geral)
+print(media_final)
