cbe90dd7624ec5382fcf5dfb12d30171d0475f4e
Kennedy <dkennyvs@gmail.com>
2023-07-11 21:26:05 -0300

    encontrar maior número

---
+++ 03_estrutura_de_repeticao/07_encontrar_maior_numero.py
@@ -0,0 +1,13 @@
+# 7. A função encontrar_maior_numero é responsável por ler os 5 números e encontrar o maior entre eles.
+# A variável contador é inicializada com 1 para controlar o número atual que está sendo lido.
+# O primeiro número é lido fora do loop while e é atribuído à variável maior_numero.
+# O loop while é executado enquanto o contador for menor que 5, indicando que ainda há números para ler.
+# Dentro do loop, os números subsequentes são lidos e armazenados na variável numero.
+# A cada iteração, verificamos se o numero lido é maior do que o maior_numero atual. Se for, atualizamos o
+# valor de maior_numero com o novo número.
+# Incrementamos o contador em 1 a cada iteração para avançar para o próximo número.
+# Após o loop, retornamos o maior_numero encontrado.
+# Na função main, chamamos a função encontrar_maior_numero para obter o maior número e, em seguida, o exibimos
+# na tela.
+# Dessa forma, o programa permite ao usuário inserir 5 números e retorna o maior número dentre eles utilizando
+# a estrutura de repetição while.
