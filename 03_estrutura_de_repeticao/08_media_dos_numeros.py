d7a9fe786d68e19d945c69c772563dc900382c79
Kennedy <dkennyvs@gmail.com>
2023-07-13 19:03:36 -0300

    Média das notas

--- 03_estrutura_de_repeticao/08_media_dos_numeros.py
+++ 03_estrutura_de_repeticao/08_media_dos_numeros.py
@@ -1,1 +1,20 @@
 # 8. Faça um programa que leia 5 números e informe a soma e a média dos números
+
+
+def media_aritmetica():
+    contador = 0
+    soma = 0
+    while contador < 5:
+        numero = float(input(f'Digite a {contador + 1}° nota: '))
+        soma += numero
+        contador += 1
+    return soma / 5
+    
+    
+def main():
+    print(f'A média das notas é: {media_aritmetica():.2f}')
+    
+    
+if __name__ == '__main__':
+    main()
+    
