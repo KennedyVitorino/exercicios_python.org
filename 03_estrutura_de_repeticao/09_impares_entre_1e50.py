bbd1c71e08e136055312b97275d896da7ddc8433
Kennedy <dkennyvs@gmail.com>
2023-07-13 20:40:44 -0300

    Ímpares entre 1 e 50

---
+++ 03_estrutura_de_repeticao/09_impares_entre_1e50.py
@@ -0,0 +1,33 @@
+# 9. Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+
+
+def sequencia_impares_while(c, n):
+    c = 0
+    n = 1
+    while c < 50 and n <= 50:
+        if n % 2 == 1:
+            print(n)
+            c += 1
+        n += 1
+
+
+# ------------------------------------------------------------------------------------------------------------
+def sequencia_impares_for(c):
+    for c in range(50):
+        if c % 2 == 1:
+            print(c)
+            c += 1
+    return c
+
+
+def main():
+    contador = 0
+    numero = 1
+    sequencia_impares_while(contador, numero)
+    
+    cont = 0
+    sequencia_impares_for(cont)
+
+
+if __name__ == '__main__':
+    main()
