734b768abadd4d49a59322e5111571a5b50586ae
Kennedy <dkennyvs@gmail.com>
2023-07-14 14:46:17 -0300

    Média aritimética 2.0

--- 01_estrutura_sequencial/05_conversor_metros_centimetros.py
+++ 01_estrutura_sequencial/05_conversor_metros_centimetros.py
@@ -6,7 +6,12 @@ def converter_metros_para_centimetros(m):
     return c
 
 
-metros = float(input('Metros à converter: '))
-centimetros = converter_metros_para_centimetros(metros)
+def main():
+    metros = float(input('Metros à converter: '))
+    centimetros = converter_metros_para_centimetros(metros)
+    
+    print(f'{metros:.1f} metros convertidos para centímetros é -> {centimetros:.1f}cm')
 
-print(f'{metros:.1f} metros convertidos para centímetros é -> {centimetros:.1f}cm')
+
+if __name__ == '__main__':
+    main()
