af25a0a6e6ff809803d640f23323e7626d4d0130
Kennedy <dkennyvs@gmail.com>
2023-07-14 14:45:30 -0300

    maior entre dois números

--- 02_estrutura_de_decisao/01_maior_entre_dois_numeros.py
+++ 02_estrutura_de_decisao/01_maior_entre_dois_numeros.py
@@ -8,8 +8,13 @@ def numero_maior(num1, num2):
     return maior
     
 
-n1 = int(input('Número 1: '))
-n2 = int(input('Número 2: '))
-maior_numero = numero_maior(n1, n2)
+def main():
+    n1 = int(input('Número 1: '))
+    n2 = int(input('Número 2: '))
+    maior_numero = numero_maior(n1, n2)
+    
+    print(maior_numero)
 
-print(maior_numero)
+
+if __name__ == '__main__':
+    main()
