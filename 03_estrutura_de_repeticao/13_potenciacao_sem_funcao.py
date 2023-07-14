5ceeaddb0e2b9d388874ecba7ad2851a71664d77
Kennedy <dkennyvs@gmail.com>
2023-07-13 22:57:50 -0300

    Potenciação sem função nativa

--- 03_estrutura_de_repeticao/13_potenciacao_sem_funcao.py
+++ 03_estrutura_de_repeticao/13_potenciacao_sem_funcao.py
@@ -1,14 +1,6 @@
 # 13. Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado
 # ao segundo número. Não utilize a função de potência da linguagem.
 
-def potenciacao_for(b, e):
-    r = 1
-    for _ in range(e):
-        r *= b
-    return r
-
-
-# ------------------------------------------------------------------------------------------------------------
 
 def potenciacao_while(b, e):
     r = 1
@@ -19,18 +11,27 @@ def potenciacao_while(b, e):
     return r
 
 
+# ------------------------------------------------------------------------------------------------------------
+
+
+def potenciacao_for(b, e):
+    r = 1
+    for _ in range(e):
+        r *= b
+    return r
+
+
 def main():
     base = int(input('Base: '))
     expoente = int(input('Expoente: '))
-    potencia = potenciacao_for(base, expoente)
+    potencia = potenciacao_while(base, expoente)
     print(f'A base {base} elevada ao expoente {expoente} é igual a = {potencia}')
     
     base1 = int(input('Base: '))
     expoente1 = int(input('Expoente: '))
-    potencia1 = potenciacao_while(base1, expoente1)
-    print(f'A base {base1} elevada ao expoente {expoente1} é igual a = {potencia1}')
+    potencia1 = potenciacao_for(base1, expoente1)
+    print(f'A base {base} elevada ao expoente {expoente1} é igual a = {potencia1}')
 
 
 if __name__ == '__main__':
     main()
-    
