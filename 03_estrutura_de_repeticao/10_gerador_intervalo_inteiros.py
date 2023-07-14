9b6ca5bc769ccb6cb5172ec7faf8fb9327963a02
Kennedy <dkennyvs@gmail.com>
2023-07-13 21:06:14 -0300

    Gerador de intervalo entre inteiros

---
+++ 03_estrutura_de_repeticao/10_gerador_intervalo_inteiros.py
@@ -0,0 +1,35 @@
+# 10. Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo
+# compreendido por eles.
+
+
+def gerar_intervalo_inteiros_while(n1, n2):
+    if n1 > n2:
+        n1, n2 = n2, n1
+    num = n1
+    while num <= n2:
+        print(num)
+        num += 1
+
+# ------------------------------------------------------------------------------------------------------------
+
+
+def gerar_intervalo_inteiros_for(n1, n2):
+    if n1 > n2:
+        n1, n2 = n2, n1
+    for num in range(n1, n2 + 1):
+        print(num)
+    
+    
+def main():
+    n1 = int(input('Digite o primeiro número: '))
+    n2 = int(input('Digite o segundo número: '))
+    gerar_intervalo_inteiros_while(n1, n2)
+    
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+    gerar_intervalo_inteiros_for(num1, num2)
+    
+    
+if __name__ == '__main__':
+    main()
+    
