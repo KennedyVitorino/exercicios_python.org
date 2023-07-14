5e5f29a457f740caaa4b1f7df712d223e261fb29
Kennedy <dkennyvs@gmail.com>
2023-07-14 14:55:04 -0300

    Conversor de Fahrenheit para Celsius.

--- 01_estrutura_sequencial/09_fahrenheit_para_celsius.py
+++ 01_estrutura_sequencial/09_fahrenheit_para_celsius.py
@@ -1,6 +1,6 @@
 # 9. Faça um Programa que peça a temperatura em graus Fahrenheit,
 # transforme e mostre a temperatura em graus Celsius.
-# C = 5 * ((F-32) / 9).
+# C = 5 * ((F-32) / 9)
 
 
 def fahrenheit_para_celsius(f):
