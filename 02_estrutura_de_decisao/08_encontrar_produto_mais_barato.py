fd87a4b10c2c7cf8bf1a62541cdd505a000a60c3
Kennedy <dkennyvs@gmail.com>
2023-07-14 14:47:04 -0300

    Encontrando produto mais barato

--- 02_estrutura_de_decisao/08_encontrar_produto_mais_barato.py
+++ 02_estrutura_de_decisao/08_encontrar_produto_mais_barato.py
@@ -9,12 +9,16 @@ def encontrar_prod_mais_barato(p1, p2, p3):
         return f'Produto 2 - R$ {p2:.2f}'
     else:
         return f'Produto 3 - R$ {p3:.2f}'
-    
-    
-preco_prod1 = float(input('Digite o preço do produto 1 -> R$ '))
-preco_prod2 = float(input('Digite o preço do produto 2 -> R$ '))
-preco_prod3 = float(input('Digite o preço do produto 3 -> R$ '))
 
-produto_mais_barato = encontrar_prod_mais_barato(preco_prod1, preco_prod2, preco_prod3)
 
-print(f'\nO produto mais barato é: {produto_mais_barato}')
+def main():
+    preco_prod1 = float(input('Digite o preço do produto 1 -> R$ '))
+    preco_prod2 = float(input('Digite o preço do produto 2 -> R$ '))
+    preco_prod3 = float(input('Digite o preço do produto 3 -> R$ '))
+    produto_mais_barato = encontrar_prod_mais_barato(preco_prod1, preco_prod2, preco_prod3)
+    
+    print(f'\nO produto mais barato é: {produto_mais_barato}')
+
+
+if __name__ == '__main__':
+    main()
