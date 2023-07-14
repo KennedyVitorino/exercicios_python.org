f39b9d42e52675b9becf1218fc6f252f6204bb05
Kennedy <dkennyvs@gmail.com>
2023-07-14 14:51:26 -0300

    Calculo de salário

--- 01_estrutura_sequencial/08_calculadora_salario.py
+++ 01_estrutura_sequencial/08_calculadora_salario.py
@@ -8,8 +8,13 @@ def calculo_salario(valor_hora, horas_trabalho):
     return salario_total
 
 
-salario_hora = float(input('Valor recebido por hora trabalhada: R$ '))
-horas_trabalhadas = int(input('Horas trabalhadas esse mês: '))
-salario_final = calculo_salario(salario_hora, horas_trabalhadas)
+def main():
+    salario_hora = float(input('Valor recebido por hora trabalhada: R$ '))
+    horas_trabalhadas = int(input('Horas trabalhadas esse mês: '))
+    salario_final = calculo_salario(salario_hora, horas_trabalhadas)
+    
+    print(f'Salário final: {salario_final:.2f}')
 
-print(f'Salário final: {salario_final:.2f}')
+
+if __name__ == '__main__':
+    main()
